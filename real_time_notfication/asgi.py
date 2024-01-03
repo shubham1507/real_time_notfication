59b72241a48f93cf8e798ef7f4b04d3319fee962
shubham joshi <shubham1507>
2024-01-03 20:02:32 +0530

    setup the notify app

---
+++ real_time_notfication/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for real_time_notfication project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from myapp.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_notfication.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+})
